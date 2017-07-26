5f85ea2d342280a2ba88e9cacbe7074935bccd85
Mac Mason <mac@kingseye.net>
2017-07-26 15:24:45 -0700

    Also bytes

--- cartographer_ros/cartographer_ros/pbstream_analyzer_main.cc
+++ cartographer_ros/cartographer_ros/pbstream_analyzer_main.cc
@@ -17,13 +17,15 @@ int main(int argc, char** argv) {
     cartographer::io::ProtoStreamReader reader(filename);
     cartographer::mapping::proto::SparsePoseGraph pose_graph;
 
+    std::cout << filename << std::endl;
+
     std::string decompressed_data;
     CHECK(reader.Read(&decompressed_data));
     google::protobuf::io::CodedInputStream stream(reinterpret_cast<const uint8*>(decompressed_data.data()), decompressed_data.size());
     stream.SetTotalBytesLimit(std::numeric_limits<int>::max(), -1);
     pose_graph.ParseFromCodedStream(&stream);
 
-    std::cout << filename << std::endl
+    std::cout << "\tBytes: " << decompressed_data.size() << std::endl
               << "\tTrajectories: " << pose_graph.trajectory_size() << std::endl
               << "\tConstraints: " << pose_graph.constraint_size() << std::endl;
 
